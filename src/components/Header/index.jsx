58c2d504f673d9711a00a2f6b5703222d5a347c2
naman995 <singhmahender3016@gmail.com>
2022-10-01 15:32:55 +0530

    header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,9 +1,28 @@
-import React from 'react';
-import image_url from './header.jpg';
-
+import React from "react";
+import image_url from "./header.jpg";
+ 
 
 const Header = () => {
-  return <div className='h-[100vh] w-auto' style={{ backgroundImage: `url(${image_url})` }}>index</div>;
-}
+  return (
+    <div
+      className="App"
+      style={{
+        backgroundImage: `url(${image_url})`,
+        minHeight: "100vh",
+        height: "auto",
+        width: "100%",
+      }}
+    >
+      <div className="h-[100vh] w-full ">
+        <p className="text-white">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Porro, quos?
+          Est voluptates voluptatem at? Nostrum sapiente tempore ut similique,
+          mollitia cumque illo, atque officia harum quasi velit, nisi illum
+          animi.
+        </p>
+      </div>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
